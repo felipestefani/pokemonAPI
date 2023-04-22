e43e8f67b7fd39d676f5aa94884197b122042f31
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-04-22 19:03:32 -0300

    adding only one button to back to home

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,12 @@
-import { Link, useNavigate } from "react-router-dom";
-import { HOME, POKEMON_DETAILS } from "../../services/urls";
+import { Link } from "react-router-dom";
+import { POKEMON_DETAILS } from "../../services/urls";
 import styles from './styles.module.css'
 
 const Card = (props) => {
-    const navigate = useNavigate()
     const {pokemon} = props
 
     return (
         <div className={`${styles.card_container}`}>
-            <button onClick={() => navigate(HOME)}>Voltar</button>
             <Link to={POKEMON_DETAILS} state={{url: pokemon.url}}>{pokemon.name}</Link>
         </div>
     )
--- src/routes/Pokemons/index.jsx
+++ src/routes/Pokemons/index.jsx
@@ -5,12 +5,16 @@ import Pagin from "../../components/Pagin";
 import CircularProgress from '@mui/material/CircularProgress';
 import BaseTemplate from "../../templates/base-template";
 import styles from './styles.module.css'
+import BackButton from "../../components/BackButton";
+import { useNavigate } from "react-router-dom";
+import { HOME } from "../../services/urls";
 
 const Pokemons = () => {
     const [pokemons, setPokemons] = useState([])
     const [loading, setLoading] = useState(false)
     const [count, setCount] = useState(0)
     const [page, setPage] = useState(localStorage.getItem('@pokemon_lastPage'))
+    const navigate = useNavigate()
     const limit = 10
     const offset = (page-1)*limit
 
@@ -37,7 +41,10 @@ const Pokemons = () => {
                 {loading ?
                     <CircularProgress style={{position:'absolute', top:'50%', left:'50%', transform:'translate(-50%, -50%)'}} />
                 :
-                <div>
+                <div className={styles.container}>
+                    <div className={styles.button_container}>
+                        <BackButton action={()=>navigate(HOME)} color={'#000'} />
+                    </div>
                     <ul className={`${styles.listContainer}`}>
                         {pokemons.map((pokemon) => (
                                 <Card pokemon={pokemon} key={pokemon.url} />
