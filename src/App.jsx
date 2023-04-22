14a745f074326ff37d577babb0691d73def2db9f
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-04-21 23:15:50 -0300

    fixing route

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <BrowserRouter>
     {/* caso deseje utilizar um template no App, as rotas devem ficar envolvidas (ou ainda deve ser visto o melhor lugar para incluir - talvez no componente) */}
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/pokemonAPI' element={<Home />} />
         <Route path='/pokemons' element={<Pokemons />} />
         <Route path='/pokemon/details' element={<PokemonDetails />} />
       </Routes>
